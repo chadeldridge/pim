21713ceabcef7f7ee7aea878d2272db66983ae05
Chad Eldridge <chad.eldridge@gmail.com>
2025-12-17 22:47:13 -0500

    Major rework to get writing to stdout and files working.

--- src/core/output.rs
+++ src/core/output.rs
@@ -1,20 +1,35 @@
 use crate::core::error::*;
+use crate::core::io::*;
+use log::debug;
 use std::{
+    cmp::Ordering,
     fmt::Debug,
-    fs::File,
     io::{IsTerminal, Write},
-    path::PathBuf,
+    path::{Path, PathBuf},
 };
 
 pub const DEFAULT_OUTPUT_FORMAT: OutputFormat = OutputFormat::Json;
 
-#[derive(Debug)]
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub enum OutputKind {
     Stdout,
     File(PathBuf),
+    Directory(PathBuf),
 }
 
-#[derive(Debug)]
+impl OutputKind {
+    pub fn new(path: &Path) -> Self {
+        if path.to_str().unwrap_or("<stdout>") == "<stdout>" {
+            OutputKind::Stdout
+        } else if path.is_dir() {
+            OutputKind::Directory(path.to_path_buf())
+        } else {
+            OutputKind::File(path.to_path_buf())
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub enum OutputFormat {
     Json,
     Yaml,
@@ -43,11 +58,11 @@ impl OutputFormat {
 }
 
 pub struct Output {
-    pub path: PathBuf,
-    pub writer: Box<dyn std::io::Write>,
-    pub kind: OutputKind,
-    pub format: OutputFormat,
-    pub pretty: bool,
+    path: PathBuf,
+    writer: Box<dyn Write>,
+    kind: OutputKind,
+    format: OutputFormat,
+    pretty: bool,
 }
 
 impl Debug for Output {
@@ -60,27 +75,51 @@ impl Debug for Output {
     }
 }
 
+impl Eq for Output {}
+impl PartialEq for Output {
+    fn eq(&self, other: &Self) -> bool {
+        (&self.path, &self.kind, &self.format, self.pretty)
+            == (&other.path, &other.kind, &other.format, other.pretty)
+    }
+}
+
+impl Ord for Output {
+    fn cmp(&self, other: &Self) -> Ordering {
+        (&self.path, &self.kind, &self.format, self.pretty).cmp(&(
+            &other.path,
+            &other.kind,
+            &other.format,
+            other.pretty,
+        ))
+    }
+}
+
+impl PartialOrd for Output {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
 impl Output {
     pub fn new(path: &PathBuf, format: OutputFormat) -> Result<Self> {
+        debug!("Creating new Output for path: {:?}", path);
         // The only time we don't pretty print is when writing to non-terminal stdout.
         let (writer, kind, pretty): (Box<dyn std::io::Write>, OutputKind, bool) =
-            if path.to_str().unwrap_or("") == "<stdout>" {
+            if path.to_str().unwrap_or("<stdout>") == "<stdout>" {
+                debug!("Outputting to stdout");
                 let is_terminal = std::io::stdout().is_terminal();
                 (Box::new(std::io::stdout()), OutputKind::Stdout, is_terminal)
             } else {
-                let file = match File::create(path) {
-                    Ok(f) => f,
-                    Err(e) => {
-                        return Err(Error::new(SourceError::Io(e)).context(&format!(
-                            "Failed to create output file: {}",
-                            path.display()
-                        )));
-                    }
-                };
+                debug!("Outputting to file: {:?}", path);
+                let w = get_writer(path)?;
                 // Files should always be written with pretty printing for readability.
-                (Box::new(file), OutputKind::File(path.clone()), true)
+                (w, OutputKind::new(path), true)
             };
 
+        debug!(
+            "Output created with kind: {:?}, format: {:?}, pretty: {}",
+            kind, format, pretty
+        );
         Ok(Output {
             path: path.clone(),
             writer,
@@ -90,17 +129,30 @@ impl Output {
         })
     }
 
-    pub fn output_format(&self) -> &OutputFormat {
+    pub fn path(&self) -> &PathBuf {
+        &self.path
+    }
+
+    pub fn writer(&mut self) -> &mut dyn Write {
+        &mut self.writer
+    }
+
+    pub fn kind(&self) -> &OutputKind {
+        &self.kind
+    }
+
+    pub fn format(&self) -> &OutputFormat {
         &self.format
     }
 
-    pub fn buf_writer(&mut self) -> &mut dyn std::io::Write {
-        &mut *self.writer
+    pub fn is_pretty(&self) -> bool {
+        self.pretty
     }
 
-    pub fn write<T: serde::Serialize>(&mut self, content: &T) -> Result<()> {
+    pub fn write<T: serde::Serialize>(&mut self, job: &str, content: &T) -> Result<()> {
         if self.pretty {
-            pretty(&mut self.writer, content, &self.format)
+            let is_stdout = matches!(self.kind, OutputKind::Stdout);
+            pretty(&mut self.writer, content, &self.format, job, is_stdout)
         } else {
             raw(&mut self.writer, content, &self.format)
         }
@@ -113,6 +165,7 @@ pub fn raw<T: serde::Serialize>(
     content: &T,
     format: &OutputFormat,
 ) -> Result<()> {
+    debug!("Writing raw output with format: {:?}", format);
     let data = match format {
         OutputFormat::Json => serde_json::to_string(content).map_err(|e| {
             Error::new(SourceError::SerdeJson(e))
@@ -126,6 +179,7 @@ pub fn raw<T: serde::Serialize>(
         })?,
     };
 
+    debug!("Writing data:\n{}", data);
     match writer.write_all(data.as_bytes()) {
         Ok(_) => Ok(()),
         Err(e) => Err(Error::new(SourceError::Io(e)).context("Failed to write output")),
@@ -137,8 +191,19 @@ pub fn pretty<T: serde::Serialize>(
     writer: &mut Box<dyn Write>,
     content: &T,
     format: &OutputFormat,
+    job: &str,
+    is_stdout: bool,
 ) -> Result<()> {
-    let data = match format {
+    debug!("Writing pretty output with format: {:?}", format);
+    let mut data = String::new();
+    if is_stdout {
+        // Notify user of output file if pretty printing. The only time we don't pretty
+        // print is when writing to non-terminal stdout. If it's pretty printing, we assume it's
+        // going to another program so we don't want to add extra output.
+        data = job.to_string() + ":\n";
+    }
+
+    let res = match format {
         OutputFormat::Json => serde_json::to_string_pretty(content).map_err(|e| {
             Error::new(SourceError::SerdeJson(e))
                 .context("Failed to serialize to JSON")
@@ -151,6 +216,12 @@ pub fn pretty<T: serde::Serialize>(
         })?,
     };
 
+    data += &res;
+    if is_stdout {
+        data += "\n";
+    }
+
+    debug!("Writing data");
     match writer.write_all(data.as_bytes()) {
         Ok(_) => Ok(()),
         Err(e) => Err(Error::new(SourceError::Io(e))
