2a22d2926a2b63a178a03ab66a38e0d83cf33333
Chad Eldridge <chad.eldridge@gmail.com>
2025-12-17 22:46:21 -0500

    Dropped shell, which is now merged with cli, and added io.

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,9 +1,9 @@
 pub use self::error::*;
 pub use self::input::{Input, InputFormat, InputKind};
+pub use self::io::*;
 pub use self::output::{Output, OutputFormat, OutputKind};
-pub use self::shell::Shell;
 
 pub mod error;
 pub mod input;
+pub mod io;
 pub mod output;
-pub mod shell;
